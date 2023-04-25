5f6eb0ba77e48a08919f55549d66e77bde443697
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-04-25 20:37:34 +0100

    added a .env file ad comments

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,14 @@
 //dependencies
 const mysql = require("mysql2");
+require("dotenv").config();
+
 //this creates the connection to the db and tells it what login details to look for
 const connection = mysql.createConnection({
   host: "127.0.0.1",
-  user: "employee_manager",
-  password: "Faith123",
-  database: "employee_manager",
+  //this allows my db login details to be encoded thereofore not as easily accessible to everyone
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 //this shows the user any errors when trying to connect or gives them a confirmation if it has been successful
